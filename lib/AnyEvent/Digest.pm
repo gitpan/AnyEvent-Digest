37022a918b26491700766e64dba8e4f821b12a1e
Yasutaka ATARASHI (新 康孝) <yakex@cpan.org>
2013-09-08 09:43:13 +0000

    Import of YAKEX/AnyEvent-Digest-v0.0.2 from CPAN.
    gitpan-cpan-distribution: AnyEvent-Digest
    gitpan-cpan-version:      v0.0.2
    gitpan-cpan-path:         YAKEX/AnyEvent-Digest-v0.0.2.tar.gz
    gitpan-cpan-author:       YAKEX
    gitpan-cpan-maturity:     released

--- lib/AnyEvent/Digest.pm
+++ lib/AnyEvent/Digest.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # ABSTRACT: A tiny AnyEvent wrapper for Digest::*
-our $VERSION = 'v0.0.1'; # VERSION
+our $VERSION = 'v0.0.2'; # VERSION
 
 use Carp;
 use AnyEvent;
@@ -168,7 +168,7 @@ AnyEvent::Digest - A tiny AnyEvent wrapper for Digest::*
 
 =head1 VERSION
 
-version v0.0.1
+version v0.0.2
 
 =head1 SYNOPSIS
 
